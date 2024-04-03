035dc4fd70bd99b2de314068fd4e6c6c3965893d
hindra2 <harold.indra@gmail.com>
2024-04-03 11:19:38 -0500

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/main.tsx"
+    }
+  },
   plugins: [react()],
 })
