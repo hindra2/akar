85d12afeb7c16cffb31ef68d1959b360e889867a
hindra2 <harold.indra@gmail.com>
2024-04-03 18:53:51 -0500

    changed sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -12,25 +12,25 @@ import HomePage from "./pages/HomePage";
 import TestPage from "./pages/TestPage";
 
 // Components imports
-import Sidebar from './components/header';
+import Sidebar from './components/sidebar';
 
-function ScrollToTop() {
-  const { pathname } = useLocation();
+// function ScrollToTop() {
+//   const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [pathname]);
 
-  return null;
-}
+//   return null;
+// }
 
 function App() {
   return (
     <Router>
-      <div className='flex flex-col min-h-screen'>
+      <div className='flex min-h-screen'>
         <Sidebar />
-        <ScrollToTop />
-        <main className='flex-grow ml-10 bg-base'>
+        {/* <ScrollToTop /> */}
+        <main className='flex-grow bg-base'>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/test" element={<TestPage />} />
