919e1056c78a7c3374fb9ad9ed6e80603c036600
wrjodjana <warrjodjana@gmail.com>
2024-04-03 21:13:44 -0500

    add client database setup

--- src/backend/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-var cors = require("cors");
-var bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
-
-const app = express();
-const port = 3000;
-const saltRounds = 10;
