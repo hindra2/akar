9f555a2159e90baf62d832d952d5d129dc021850
hindra2 <harold.indra@gmail.com>
2024-04-15 19:21:45 -0500

    added pomodoro timer values to backend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { PomodoroProvider } from './components/Pomodoro/PomodoroContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PomodoroProvider>
+      <App />
+    </PomodoroProvider>
   </React.StrictMode>,
 )
