243849eb87adf679f5fc2cc02effa6d5ce59299e
wrjodjana <warrjodjana@gmail.com>
2024-04-20 18:56:00 -0500

    changed local host to jonathan's ip

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   password: "1234",
-  host: "localhost",
+  host: "10.193.31.211",
   port: 5432,
   database: "akar",
 });
