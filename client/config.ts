a3d48453ae5c3367ebc500d51e30a3d2da025917
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-04-22 23:10:29 -0500

    fixed firebase issue

---
+++ client/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "your_api_key",
+  authDomain: "your_auth_domain",
+  projectId: "your_project_id",
+  storageBucket: "your_storage_bucket",
+  messagingSenderId: "your_sender_id",
+  appId: "your_app_id",
+  measurementId: "your_measurement_id",
+};
