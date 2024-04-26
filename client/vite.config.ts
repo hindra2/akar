7a3646c870a0cb47a8a43de73a8d76d9d217db7a
hindra2 <harold.indra@gmail.com>
2024-04-25 20:48:58 -0500

    updated vite config and index.html

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   root: 'client',
   build: {
-    outDir: './dist',
+    outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
       input: './index.html',
