e45c9c18ec29554c7b0ee185e7ebcbad6939617e
hindra2 <harold.indra@gmail.com>
2024-05-14 16:23:26 -0500

    added vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,13 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: "./index.html",
-    },
-  },
-  plugins: [react()],
   base: "/akar/",
-});
+  plugins: [react()],
+  build: {
+    outDir: './dist'
+  }
+})
+
