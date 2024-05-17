4b62724cb46e05ae2b83ba6d4efb9350d5eb7bf8
hindra2 <harold.indra@gmail.com>
2024-05-17 17:13:58 -0500

    updated main.yml

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/akar/",
+  base: "./",
   plugins: [react()],
   build: {
     outDir: './dist'
