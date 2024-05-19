93a3646b6c71fc096360862c5f05f497650bce20
hindra2 <harold.indra@gmail.com>
2024-05-19 03:38:18 -0500

    added login supabase

--- client/utils/supabase.ts
+++ client/utils/supabase.ts
@@ -1,9 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://ldyytltiuekjyuketpzm.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkeXl0bHRpdWVranl1a2V0cHptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU5OTk3MTIsImV4cCI6MjAzMTU3NTcxMn0.90VUxXQpagNneWy1UJ7t4zDRfJMgqjlYV4pQOdheWB4";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
-        
