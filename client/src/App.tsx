f18b19c924c6b774f86546ad28d9b3f0206a1701
hindra2 <harold.indra@gmail.com>
2024-05-21 16:03:26 -0500

    updated app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import {
   Routes,
   Route,
   useLocation,
-  Navigate
+  Navigate,
 } from "react-router-dom";
 import { useEffect } from "react";
 import supabase from "../utils/supabase";
@@ -22,6 +22,7 @@ import Sidebar from "./components/SideBar/sidebar";
 import CardView from "./pages/CardView";
 import AddCard from "./pages/AddCard";
 import LandingPage from "./pages/Landing";
+import ManageCards from "./pages/ManageCards";
 
 // Type for children prop
 interface LayoutWithSidebarProps {
@@ -39,7 +40,11 @@ function LayoutWithSidebar({ children, onLogout }: LayoutWithSidebarProps) {
 
   return (
     <div className="flex min-h-screen overflow-hidden bg-base">
-      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} onLogout={onLogout}/>
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        toggleSidebar={toggleSidebar}
+        onLogout={onLogout}
+      />
       <main
         className={`flex-grow bg-base transition-all duration-300 ease-in-out ${
           isSidebarOpen ? "ml-64" : "ml-0"
@@ -66,47 +71,51 @@ const App: React.FC = () => {
 
   const handleLogin = () => {
     setIsLoggedIn(true);
-  }
+  };
 
   const handleLogout = () => {
     localStorage.removeItem("user");
     setIsLoggedIn(false);
-  }
+  };
 
   useEffect(() => {
     const checkSession = async () => {
-      const { data: { session } } = await supabase.auth.getSession();
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
       setIsLoggedIn(session !== null);
     };
-  
+
     checkSession();
-  
-    const { data: { subscription } } = supabase.auth.onAuthStateChange(async (_, session) => {
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(async (_, session) => {
       setIsLoggedIn(session !== null);
     });
-  
+
     const applyTheme = (theme: string) => {
       document.documentElement.classList.remove("dark", "light");
       document.documentElement.classList.add(theme);
     };
-  
+
     const storedTheme = localStorage.getItem("theme");
     const prefersDark = window.matchMedia(
       "(prefers-color-scheme: dark)"
     ).matches;
-  
+
     const theme = storedTheme || (prefersDark ? "dark" : "light");
     applyTheme(theme);
-  
+
     const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
     const handleChange = (e: MediaQueryListEvent) => {
       const newTheme = e.matches ? "dark" : "light";
       applyTheme(newTheme);
       localStorage.setItem("theme", newTheme);
     };
-  
+
     mediaQuery.addEventListener("change", handleChange);
-  
+
     return () => {
       subscription.unsubscribe();
       mediaQuery.removeEventListener("change", handleChange);
@@ -121,31 +130,41 @@ const App: React.FC = () => {
           <div>Loading...</div>
         ) : isLoggedIn ? (
           <>
-            <Route path="/" element={
-              <LayoutWithSidebar onLogout={handleLogout}>
-                <HomePage />
-              </LayoutWithSidebar>
-            }
+            <Route
+              path="/"
+              element={
+                <LayoutWithSidebar onLogout={handleLogout}>
+                  <HomePage />
+                </LayoutWithSidebar>
+              }
             />
-            <Route path="/statistics" element={
+            <Route
+              path="/statistics"
+              element={
                 <LayoutWithSidebar onLogout={handleLogout}>
                   <Statistics />
                 </LayoutWithSidebar>
               }
             />
-            <Route path="/settings" element={
+            <Route
+              path="/settings"
+              element={
                 <LayoutWithSidebar onLogout={handleLogout}>
                   <Settings />
                 </LayoutWithSidebar>
               }
             />
-            <Route path="/pomodoro" element={
+            <Route
+              path="/pomodoro"
+              element={
                 <LayoutWithSidebar onLogout={handleLogout}>
                   <Pomodoro />
                 </LayoutWithSidebar>
               }
             />
-            <Route path="/deckinfo" element={
+            <Route
+              path="/deckinfo"
+              element={
                 <LayoutWithSidebar onLogout={handleLogout}>
                   <DeckInfo />
                 </LayoutWithSidebar>
@@ -159,20 +178,33 @@ const App: React.FC = () => {
                 </LayoutWithSidebar>
               }
             />
-            <Route path="/addcard" element={
+            <Route
+              path="/addcard"
+              element={
                 <LayoutWithSidebar onLogout={handleLogout}>
                   <AddCard />
+                </LayoutWithSidebar>
+              }
+            />
+            <Route
+              path="/managecards"
+              element={
+                <LayoutWithSidebar onLogout={handleLogout}>
+                  <ManageCards />
                 </LayoutWithSidebar>
               }
             />
             <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
             <Route path="/landing" element={<Navigate to="/" replace />} />
           </>
-        ):(
+        ) : (
           <>
             <Route path="/landing" element={<LandingPage />} />
             <Route path="/" element={<Navigate to="/landing" replace />} />
-            <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+            <Route
+              path="/login"
+              element={<LoginPage onLogin={handleLogin} />}
+            />
           </>
         )}
       </Routes>
