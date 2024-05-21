05e18112fe25eb3ddaec091f198f8b8969c6ada7
hindra2 <harold.indra@gmail.com>
2024-05-21 15:59:48 -0500

    changed app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -166,6 +166,7 @@ const App: React.FC = () => {
               }
             />
             <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+            <Route path="/landing" element={<Navigate to="/" replace />} />
           </>
         ):(
           <>
